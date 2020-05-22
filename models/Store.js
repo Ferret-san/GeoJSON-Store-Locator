39877d139edebdeb62789543ce178e067a59a7d6
Diego Ferrer <31937514+Ferret-san@users.noreply.github.com>
2020-05-22 13:19:00 -0400

    Add files via upload

---
+++ models/Store.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose');
+const geocoder = require('../utils/geocoder');
+
+const StoreSchema = new mongoose.Schema({
+    storeID: {
+        type: String,
+        required: [true, 'Please add a store ID'],
+        unique: true,
+        trim: true,
+        maxlength: [10, 'Store ID must be less than 10 chars']
+    },
+    address: {
+        type: String,
+        required: [true, 'Please add an addres']
+    },
+    location: {
+        type: {
+          type: String, // Don't do `{ location: { type: String } }`
+          enum: ['Point'], // 'location.type' must be 'Point'
+        },
+        coordinates: {
+          type: [Number],
+          index: '2dsphere' //2dsphere supports queries that calculate geometries on an earth-like sphere
+        },
+        formattedAddress: String
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+// Geocode & create location
+StoreSchema.pre('save', async function(next) {
+    const loc = await geocoder.geocode(this.address);
+    this.location = {
+        type: 'Point',
+        coordinates: [loc[0].longitude, loc[0].latitude],
+        formattedAddress: loc[0].formattedAddress
+    }
+
+    // Do not save address
+    this.address = undefined;
+    next();
+});
+
+module.exports = mongoose.model('Store', StoreSchema);
